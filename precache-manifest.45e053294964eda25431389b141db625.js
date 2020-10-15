0b8f137d3d5dd208312cd7f7f93dbd0b62bb2c21
Pradeep <pradeepscomputer@gmail.com>
2020-10-15 10:16:57 +0530

    Updates

---
+++ precache-manifest.45e053294964eda25431389b141db625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb67e2be873a793c31b14e43bcd1038c",
+    "url": "/color-generator/index.html"
+  },
+  {
+    "revision": "2610de26564651d65590",
+    "url": "/color-generator/static/css/main.1288c749.chunk.css"
+  },
+  {
+    "revision": "bbd34c77df1400a4866e",
+    "url": "/color-generator/static/js/2.9a9297df.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/color-generator/static/js/2.9a9297df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2610de26564651d65590",
+    "url": "/color-generator/static/js/main.2eee9003.chunk.js"
+  },
+  {
+    "revision": "09d8813e4831161fabce",
+    "url": "/color-generator/static/js/runtime-main.19dd4eca.js"
+  }
+]);
